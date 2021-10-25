521300920734df8809f60c3d50b38cfe602506cf
Fernando Lira <it.fernandolira@gmail.com>
2021-10-25 16:37:48 +0100

    Disconnect

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ io.on("connection",(socket) => {
     //console.log(socket);
     //console.log(socket.id);
 
+    socket.on("disconnect",() =>{
+        console.log(socket.id + " disconnected");
+    });
+
     socket.on("bemvindo",(data) => {
         console.log(data);
     });
