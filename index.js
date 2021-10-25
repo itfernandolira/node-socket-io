f4829aa0c645ca40c2ce857853c336b40b88c952
Fernando Lira <it.fernandolira@gmail.com>
2021-10-25 16:10:54 +0100

    Connecting backend to frontend

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var io=require("socket.io")(http);
 
 app.set("view engine", "ejs");
 
+//Criar o evento Connection
+//socket é o cliente - uma instância por cada cliente
+io.on("connection",(socket) => {
+    console.log(socket);
+    console.log(socket.id);
+});
+
 app.get("/",(req,res) => {
     res.render("index");
 });
