3bc791d024bcbd498fdaf68785866bf96d6f1515
Fernando Lira <it.fernandolira@gmail.com>
2021-10-25 17:06:08 +0100

    Chat example - Global emit

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ io.on("connection",(socket) => {
 
     socket.on("msg", (data) => {
         console.log(data);
-        socket.emit("showmsg",data);
+        //socket.emit("showmsg",data);
+        io.emit("showmsg",data);
     })
 
 });
